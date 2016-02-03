9e265d4f087eaad38a1dd8f6904e6cb79455a722
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-03 14:12:50 +0100

    Port extension loader class to Bolt v3 API

--- src/BaseWidgetExtension.php
+++ src/BaseWidgetExtension.php
@@ -2,26 +2,27 @@
 
 namespace Bolt\Extension\Bolt\BaseWidget;
 
-use Bolt\Application;
-use Bolt\BaseExtension;
+use Bolt\Asset\Widget\Widget;
+use Bolt\Extension\SimpleExtension;
 
-class BaseWidgetExtension extends BaseExtension
+class BaseWidgetExtension extends SimpleExtension
 {
-
-    public function initialize()
+    protected function registerAssets()
     {
-        foreach((array) $this->config['widgets'] as $name => $widget) {
+        $widgets = [];
+        $config = $this->getConfig();
+        foreach ((array) $config['widgets'] as $name => $widget) {
 
-            // Skip diabled extensions.
+            // Skip disabled extensions.
             if (isset($widget['enabled']) && $widget['enabled'] === false) {
                 continue;
             }
 
-            if(empty($widget['zone']) || $widget['zone'] !== 'backend') {
+            if (empty($widget['zone']) || $widget['zone'] !== 'backend') {
                 $widget['zone'] = 'frontend';
             }
 
-            $widgetObj = new \Bolt\Asset\Widget\Widget();
+            $widgetObj = new Widget();
             $widgetObj
                 ->setZone($widget['zone'])
                 ->setLocation($widget['location'])
@@ -41,19 +42,16 @@ class BaseWidgetExtension extends BaseExtension
                 $widgetObj->setCacheDuration($widget['cacheduration']);
             }
 
-            // dump($widgetObj);
-
-            $this->addWidget($widgetObj);
+            $widgets[] = $widgetObj;
         }
-    }
 
-    public function getName()
-    {
-        return "basewidget";
+        return $widgets;
     }
 
     public function widget($widget)
     {
+        $app = $this->getContainer();
+
         // If we need a 'static' widget, we can just retun the output here and we're done
         if (!empty($widget['static'])) {
             return $widget['static'];
@@ -68,38 +66,29 @@ class BaseWidgetExtension extends BaseExtension
         if (!empty($widget['record'])) {
             list($ct, $slug) = explode('/', $widget['record']);
             $key = is_numeric($slug) ? 'id' : 'slug';
-            $record = $this->app['storage']->getContent($ct, [$key => $slug, 'returnsingle' => true]);
+            $record = $app['storage']->getContent($ct, [$key => $slug, 'returnsingle' => true]);
         } else {
             $record = $widget['content'];
         }
 
         // Data to pass into the widget
         $data = [
-            'record' => $record,
-            'widget' => $widget,
-            'content' => $widget['content']
+            'record'  => $record,
+            'widget'  => $widget,
+            'content' => $widget['content'],
         ];
-
-        // Add the `widgets/` path, so it can be overridden in themes
-        $this->app['twig.loader.filesystem']->addPath(__DIR__);
 
         // Make sure 'template' is defined, and the template file exists
         if (empty($widget['template'])) {
-            return "<strong>Base Widget error:</strong> Custom widgets need to define a template.";
-        } elseif (!$this->app['twig.loader']->exists($widget['template'])) {
-            return "<strong>Base Widget error:</strong> Widget template '<tt>" . $widget['template'] .
-                "</tt>' does not exist, or isn't readable.";
+            return '<strong>Base Widget error:</strong> Custom widgets need to define a template.';
         }
 
-        // Render the template, and return the results
-        return $this->app['render']->render($widget['template'], $data);
+        try {
+            // Render the template, and return the results
+            return $this->renderTemplate($widget['template'], $data);
+        } catch (\Twig_Error_Loader $e) {
+            return "<strong>Base Widget error:</strong> Widget template '<tt>" . $widget['template'] .
+            "</tt>' does not exist, or isn't readable.";
+        }
     }
-
-
 }
-
-
-
-
-
-
